392c6710948a61becf9ee6a55e3e8f40c01828d4
odalys52 <omartinezvilla7769@eagle.fgcu.edu>
2019-12-02 19:20:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{rollDice()}
+    }
+
+    private fun rollDice(){
+        val randomInt = Random().nextInt(6) + 1
+        result_text.text = randomInt.toString()
+        //Toast.makeText(this, "button clicked",
+            //Toast.LENGTH_SHORT).show()
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "Dice Rolled!"
+    }
+}
